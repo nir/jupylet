2b32ad27de8052051376c839a0ed18d95e5c2218
Nir Aides <naides@shutterfly.com>
2020-01-17 12:09:13 +0200

    Handle resource loading with autocrop

--- jupylet/sprite.py
+++ jupylet/sprite.py
@@ -29,56 +29,7 @@ import pyglet
 
 import PIL.Image
 
-import numpy as np
-
-
-def image_from(o, width=None, height=None, crop=True):
-    
-    if type(o) is str:
-        return image_from_resource(o)
-    
-    if isinstance(o, np.ndarray):
-        #return image_from_array(o)
-        o = PIL.Image.fromarray(o.astype('uint8'))
-    
-    if isinstance(o, PIL.Image.Image):
-        return image_from_pil(o)
-    
-    return o
-
-    
-def image_from_resource(name):
-    
-    try:
-        return pyglet.resource.image(name)
-    
-    except pyglet.resource.ResourceNotFoundException:
-        pyglet.resource.reindex()
-        return pyglet.resource.image(name)
-
-    
-def image_from_array(array):
-    
-    array = array.astype('uint8')
-
-    height, width = array.shape[:2]
-    
-    if len(array.shape) == 2:
-        channels = 1
-    else:
-        channels = array.shape[-1]
-        
-    mode = ['L', None, 'RGB', 'RGBA'][channels-1]
-        
-    return pyglet.image.ImageData(width, height, mode, array.reshape(-1).tobytes(), -width*channels)
-
-
-def image_from_pil(image):
-    
-    width, height = image.size 
-    channels = {'L': 1, 'RGB': 3, 'RGBA': 4}[image.mode]
-        
-    return pyglet.image.ImageData(width, height, image.mode, image.tobytes(), -width*channels)
+from .resource import image_from
 
 
 class Sprite(pyglet.sprite.Sprite):
@@ -91,9 +42,16 @@ class Sprite(pyglet.sprite.Sprite):
                  group=None,
                  usage='dynamic',
                  subpixel=True,
+                 autocrop=False,
+                 width=None,
+                 height=None,
+                 anchor_x='center',
+                 anchor_y='center',
                  scale=1.0):
 
-        img = image_from(img)
+        self.autocrop = autocrop
+
+        img = image_from(img, autocrop=self.autocrop)
             
         super(Sprite, self).__init__(img, x, y,
                  blend_src,
@@ -103,10 +61,16 @@ class Sprite(pyglet.sprite.Sprite):
                  usage,
                  subpixel)
 
-        self.anchor_x = 'center'
-        self.anchor_y = 'center'
-        
+        if width:
+            self.width = width
+
+        elif height:
+            self.height = height
+
+        self.anchor_x = anchor_x
+        self.anchor_y = anchor_y        
         self.scale = scale
+
         self._update_position()
 
     @property
@@ -122,7 +86,7 @@ class Sprite(pyglet.sprite.Sprite):
         anchor_x = self.anchor_x / self.width
         anchor_y = self.anchor_y / self.height
 
-        img = image_from(img)
+        img = image_from(img, autocrop=self.autocrop)
             
         pyglet.sprite.Sprite.image.fset(self, img)
 
